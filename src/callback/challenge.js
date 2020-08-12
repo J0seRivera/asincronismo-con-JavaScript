746c0ec416c98365a4f7c91a8b50466f22abb003
Jose-Rivera <joseriveradl@gmail.com>
2020-08-11 23:00:45 -0600

    challenge peticiones a una api con callbacks

---
+++ src/callback/challenge.js
@@ -0,0 +1,36 @@
+//instancia de xmlhttprequest para hacer peticiones a nuestra api
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
+
+//funcion que recibe la api a la que nos conectaremos y un callback
+function fetchData(url_api, callback) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.open('GET', url_api, true);
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error('Error ' + url_api);
+        return callback(error, null);
+      }
+    }
+  }
+  xhttp.send();
+}
+//llamadasa la api
+fetchData(API, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(data2.origin.url, function (error3, data3) {
+      if (error3) return console.error(error3);
+      //callback hell, peticiones encadenadas es una mala practica como desarrollador LOL
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  })
+
+
+})
